5793a02ac1aada76b89ea0bb988528e5cee2e92b
Michael Ireland <mireland@malice.anu.edu.au>
2016-08-23 23:38:22 +1000

    Updated the HD169142 defaults

--- go_HD169142.py
+++ go_HD169142.py
@@ -14,14 +14,14 @@ nwalkers = 24
 print('nwalkers=',nwalkers)
 
 #set parameters to 0.0 if you don't want them investigated. Have to set ipar_sig to zero also!
-ipar = np.array([np.log(0.6e-3), #Dust to gas
-                 np.log(3e-3), #Gap Depletion
-                 np.log(16.0),     #Inner Radius (AU)
+ipar = np.array([np.log(0.58e-3), #Dust to gas
+                 np.log(2.3e-3), #Gap Depletion
+                 np.log(12.7),     #Inner Radius (AU)
                  np.log(25.0),     #Wall Radius (AU)
-                 20,               #Inclination
-                 90,               #Position Angle
+                 30,               #Inclination
+                 138,               #Position Angle
                  0.0,0.0,          #Star position offset (AU)
-                 np.log(7300.0),   #Stellar temperature (8250 from Yeon Seok 2015)
+                 np.log(7000.0),   #Stellar temperature (8250 from Yeon Seok 2015)
                  0.0,0.0,0.0])     #Planet x, y, radius.
 
 ntest = 9
@@ -41,18 +41,18 @@ if (False):
         lnprob[i] = lnprob_conv_disk_radmc3d(ipar_test, dist=145, remove_directory=False)
     pdb.set_trace()
                  
-ipar_sig = np.array([.01,.01,.01,.0,1,5,0.,0.,0.05,0.,0.,0.0])
+ipar_sig = np.array([.01,.01,.01,.0,1,5,0.05,0.05,0.05,0.,0.,0.0])
 ndim = len(ipar)
 #Could use parameters of random.normal instead of below, if you prefer that.
 p0 = [ipar + np.random.normal(size=ndim)*ipar_sig for i in range(nwalkers)]
 
 if (multiprocess):
     threads = multiprocessing.cpu_count()
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob_conv_disk_radmc3d,threads=threads,kwargs={"planet_temp":2000})
-    sampler.run_mcmc(p0,5)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob_conv_disk_radmc3d,threads=threads,kwargs={"planet_temp":2000,"temperature":1000,"dist":145.})
+    sampler.run_mcmc(p0,500)
 else:
     sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob_conv_disk_radmc3d,kwargs={"planet_temp":2000})
-    sampler.run_mcmc(p0,5)
+    sampler.run_mcmc(p0,50)
 
 chainfile = open('chainfile.pkl','w')
 pickle.dump((sampler.lnprobability,sampler.chain),chainfile)
