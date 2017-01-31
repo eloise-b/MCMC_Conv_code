0d7f3f7ebe62c1029528e47debeb68cc56c076f4
eloise-b <eloise-b@users.noreply.github.com>
2017-01-31 14:04:50 +1100

    adding dust to gas prior

--- mcinmc/lnprob_radmc3d.py
+++ mcinmc/lnprob_radmc3d.py
@@ -337,9 +337,9 @@ def lnprior(x):
             'r_dust':np.exp(x[3]),'r_in':np.exp(x[4]),'r_wall':np.exp(x[5]),'inc':x[6],\
             'pa_sky':x[7],'star_x':x[8],'star_y':x[9],'planet_x':x[10], 'planet_y':x[11],\
             'planet_r':x[12]}
-    if params['r_in'] < params['r_wall'] and params['r_wall'] < 60. and \
+    if params['r_in'] < params['r_wall'] and params['r_wall'] < out_wall. and \
        params['r_wall'] > params['r_in']+0.1 and params['r_dust'] < params['r_in'] and \
-       params['r_dust']>0.1:
+       params['r_dust'] > 0.1 and params['dtog'] < 5000:
         return 0.0
     return -np.inf
    
