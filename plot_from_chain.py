d8015bf315658fd7b49ad1071327a45a29d3e969
eloise-b <eloise-b@users.noreply.github.com>
2017-02-21 11:59:33 +1100

    put option to plot as if statement

--- plot_from_chain.py
+++ plot_from_chain.py
@@ -21,9 +21,14 @@ import pickle
 
 complete = True
 #if the chain did not complete, use join_chain.py to make the chain_file.txt
+plot_results = False
+im_num = 15.
+temp=10000.
 #the names of the parameters
 names=['d_to_g','gap_dep_1','gap_dep_2','r_dust','r_in','r_wall','inclination','pa','star_x','star_y',\
        'planet_x','planet_y','planet_r']
+#value=[d_to_g,gap_dep_1,gap_dep_2,r_dust,r_in,r_wall,inclination,pa,star_x,star_y,\
+#       planet_x,planet_y,planet_r]
 
 if complete:
     file = open('chainfile.pkl','r')
@@ -34,7 +39,7 @@ if complete:
     flatchain = np.reshape(chain, (26000,13))
 
     print("minimum ln prob =", np.max(flatprob))
-    print("this is a chi-squared of ", np.max(flatprob)/128./128./15*2*10000) 
+    print("this is a chi-squared of ", np.max(flatprob)/128./128./im_num*2*temp) 
     #10000 is the temperature, change this to the temperature that was actually used
 
     print("the chain at this point is (with the first 6 ln) ", flatchain[np.argmax(flatprob)])
@@ -49,41 +54,41 @@ if complete:
         if i < 6:
             samples[:,i] = np.exp(samples[:,i])
         value = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-                             zip(*np.percentile(samples[:,i], [16, 50, 84],
+                             zip(*np.percentile(samples, [16, 50, 84],
                                                 axis=0)))
-        print(names[i], ' has value and errors: ', value)
+        print(names[i], ' has value and errors: ', value[i])
     
-    os.makedirs("param_vs_param")
-    
-    #Plot the MC threads
-    plt.plot(prob.T)
-    plt.title("MC Threads")
-    plt.xlabel( 'Iterations')
-    plt.ylabel('ln probability')
-    plt.savefig('MCMC_thread.eps')
-    plt.clf()    
+    if plot_results:
+        os.makedirs("param_vs_param")
+        #Plot the MC threads
+        plt.plot(prob.T)
+        plt.title("MC Threads")
+        plt.xlabel( 'Iterations')
+        plt.ylabel('ln probability')
+        plt.savefig('MCMC_thread.eps')
+        plt.clf()    
 
-    #Plot each of the parameters against each other
-    for i,k in zip(param_number,names):
-        for j,l in zip(param_number,names):
-            if i!=j:
-                if (i in [6:13]) and (j in [6:13]):
-                    plt.plot((flatchain[:,i]), (flatchain[:,j]),'.')
-                elif i in [6:13]:
-                    plt.plot((flatchain[:,i]), np.exp(flatchain[:,j]),'.')
-                elif j in [6:13]:
-                    plt.plot(np.exp(flatchain[:,i]), (flatchain[:,j]),'.')
-                else:
-                    plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
+        #Plot each of the parameters against each other
+        for i,k in zip(param_number,names):
+            for j,l in zip(param_number,names):
+                if i!=j:
+                    if (i in range(6,13)) and (j in range(6,13)):
+                        plt.plot((flatchain[:,i]), (flatchain[:,j]),'.')
+                    elif i in range(6,13):
+                        plt.plot((flatchain[:,i]), np.exp(flatchain[:,j]),'.')
+                    elif j in range(6,13):
+                        plt.plot(np.exp(flatchain[:,i]), (flatchain[:,j]),'.')
+                    else:
+                        plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
 
-                #plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
-                title = k + ' vs ' + l
-                im_name = "param_vs_param/"+k+"_vs_"+l+".eps"
-                plt.title(title)
-                plt.xlabel(k)
-                plt.ylabel(l)
-                plt.savefig(im_name)
-                plt.clf()
+                    #plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
+                    title = k + ' vs ' + l
+                    im_name = "param_vs_param/"+k+"_vs_"+l+".eps"
+                    plt.title(title)
+                    plt.xlabel(k)
+                    plt.ylabel(l)
+                    plt.savefig(im_name)
+                    plt.clf()
                 
     
 else:
@@ -95,28 +100,45 @@ else:
     param_number = np.arange(0,len(flatchain[1]))
     flatchain = flatchain
     
-    os.makedirs("param_vs_param")
-    
-    #Plot each of the parameters against each other
-    for i,k in zip(param_number,names):
-        for j,l in zip(param_number,names):
-            if i!=j:
-                if (i in [6:13]) and (j in [6:13]):
-                    plt.plot((flatchain[:,i]), (flatchain[:,j]),'.')
-                elif i in [6:13]:
-                    plt.plot((flatchain[:,i]), np.exp(flatchain[:,j]),'.')
-                elif j in [6:13]:
-                    plt.plot(np.exp(flatchain[:,i]), (flatchain[:,j]),'.')
-                else:
-                    plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
+    print("minimum ln prob =", np.max(flatprob))
+    print("this is a chi-squared of ", np.max(flatprob)/128./128./im_num*2*temp) 
+    #10000 is the temperature, change this to the temperature that was actually used
 
-                #plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
-                title = k + ' vs ' + l
-                im_name = "param_vs_param/"+k+"_vs_"+l+".eps"
-                plt.title(title)
-                plt.xlabel(k)
-                plt.ylabel(l)
-                plt.savefig(im_name)
-                plt.clf()
+    print("the chain at this point is ", flatchain[np.argmax(flatprob)])
+    
+    #find the errors for each of the parameters and the appropriate value to quote
+    for i in param_number:
+        samples = flatchain
+        if i < 6:
+            samples[:,i] = np.exp(samples[:,i])
+        value = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+                             zip(*np.percentile(samples, [16, 50, 84],
+                                                axis=0)))
+        print(names[i], ' has value and errors: ', value[i])
+    
+    if plot_results:
+        os.makedirs("param_vs_param")
+    
+        #Plot each of the parameters against each other
+        for i,k in zip(param_number,names):
+            for j,l in zip(param_number,names):
+                if i!=j:
+                    if (i in range(6,13)) and (j in range(6,13)):
+                        plt.plot((flatchain[:,i]), (flatchain[:,j]),'.')
+                    elif i in range(6,13):
+                        plt.plot((flatchain[:,i]), np.exp(flatchain[:,j]),'.')
+                    elif j in range(6,13):
+                        plt.plot(np.exp(flatchain[:,i]), (flatchain[:,j]),'.')
+                    else:
+                        plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
+
+                    #plt.plot(np.exp(flatchain[:,i]), np.exp(flatchain[:,j]),'.')
+                    title = k + ' vs ' + l
+                    im_name = "param_vs_param/"+k+"_vs_"+l+".eps"
+                    plt.title(title)
+                    plt.xlabel(k)
+                    plt.ylabel(l)
+                    plt.savefig(im_name)
+                    plt.clf()
             
             
